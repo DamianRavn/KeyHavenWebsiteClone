f1055498d5717a38c13957168126f01cdc44a51a
Damian Ravn <DamianRavn@gmail.com>
2018-09-17 11:12:35 +0200

    added link

--- key_haven_publishing/src/App.js
+++ key_haven_publishing/src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <a href="http://www.khpplc.co.uk/" title="Key Haven Publications Ltd.">Key Haven Publications Ltd.</a>
       </div>
     );
   }
